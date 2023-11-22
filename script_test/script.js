80490d2903f712c9d31a6f742106356444f3942c
ngochai1242003 <dangngochai120403@gmail.com>
2023-11-22 22:09:28 +0700

    update code

---
+++ script_test/script.js
@@ -0,0 +1,43 @@
+// var a = 3; //cú pháp cũ
+// const b = 4; //khai báo hằng số
+// let c = 5; //khai báo biến
+
+//--------------------------------------------------------------------------------
+
+// var firstName;
+// var middleName;
+// var lastName;
+
+
+// firstName= 'Dang'
+// middleName= 'Ngoc'
+// lastName= 'Hai'
+
+// var fullname = firstName + ' ' + middleName + ' ' + lastName;
+
+// console.log(fullname);
+
+
+// var age;
+// age= '20'
+// var profile = 'Tôi tên là: ' + lastName + '.' + ' Tên đầy đủ là ' + fullname + '.' + ' Năm nay tôi ' + age + ' tuổi.';
+// console.log(profile);
+
+//--------------------------------------------------------------------------------
+
+var a=5;
+var b='5';
+// == => GIá trị bằng nhau thì => true (không quan tâm kiểu dữ liệu)
+console.log("a == b", a==b);
+
+console.log("a === b",a===b);
+// != => GIá trị bằng nhau thì => false (không quan tâm kiểu dữ liệu)
+console.log("a != b", a!=b);
+console.log('a !== b',a!==b);
+console.log('a && b',a && b);
+
+
+
+
+
+
